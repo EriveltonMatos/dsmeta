3bb962d029a6b8c3c542a1d7be3e9be4f9840a80
Erivelton Matos <eriveltonmatos@hotmail.com>
2022-11-18 09:05:12 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,7 +1,89 @@
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import NotificationButton from '../NotificationButton'
+import './styles.css'
 function SalesCard() {
   return (
-     <h1>Sales Card</h1>
+    <div className="dsmeta-card">
+      <h2 className="dsmeta-sales-title">Vendas</h2>
+      <div>
+        <div className="dsmeta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className="dsmeta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+      </div>
+
+      <div>
+        <table className="dsmeta-sales-table">
+          <thead>
+            <tr>
+              <th className="show992">ID</th>
+              <th className="show576">Data</th>
+              <th>Vendedor</th>
+              <th className="show992">Visitas</th>
+              <th className="show992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th className="show992">ID#341</th>
+              <th className="show576">08/07/2022</th>
+              <th>Anakin</th>
+              <th className="show992">15</th>
+              <th className="show992">11</th>
+              <th>R$ 55300.00</th>
+              <th>
+                <div className="dsmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </th>
+            </tr>
+            <tr>
+              <th className="show992">ID#341</th>
+              <th className="show576">08/07/2022</th>
+              <th>Anakin</th>
+              <th className="show992">15</th>
+              <th className="show992">11</th>
+              <th>R$ 55300.00</th>
+              <th>
+                <div className="dsmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </th>
+            </tr>
+            <tr>
+              <th className="show992">ID#341</th>
+              <th className="show576">08/07/2022</th>
+              <th>Anakin</th>
+              <th className="show992">15</th>
+              <th className="show992">11</th>
+              <th>R$ 55300.00</th>
+              <th>
+                <div className="dsmeta-red-btn-container">
+                  <NotificationButton />
+                </div>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
